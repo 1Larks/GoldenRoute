e0a8efdc08f481c1c505e71b45e318ce705142d7
1Larks <oribenado06@gmail.com>
2024-05-12 11:09:04 +0300

    everything except DB

--- Backend/Controllers/planeController.js
+++ Backend/Controllers/planeController.js
@@ -1,7 +1,8 @@
 import { get_formatted_aircrafts } from "../Services/planeService.js";
 
 const getAircrafts = async (req, res) => {
-    const aircrafts = await get_formatted_aircrafts(req.params.latitude, req.params.longitude, req.params.radius);
+    const aircrafts = await get_formatted_aircrafts(req.params.latitude, req.params.longitude, 
+        req.params.radius, req.params.velocity);
     return res.json(aircrafts);
 }
 
