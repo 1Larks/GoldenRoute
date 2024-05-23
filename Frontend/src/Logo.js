316a1395b403a2eb1af98ec51eee67e889486023
1Larks <oribenado06@gmail.com>
2024-05-24 00:17:59 +0300

    Added "Operation" on top of the other text

--- Frontend/src/Logo.js
+++ Frontend/src/Logo.js
@@ -1,12 +1,13 @@
 import './App.css';
-import airforce from './images/airforce.png';
+import airforce from './Images/airforce.png';
 
 const Logo = () => {
 
     return (<div className='logoContainer'>
           <div className='textContainer'>
-              <h1>Angels of Heaven</h1>
-              <h2>Unmanned Aerial Vehicle Information Interface</h2>
+            <h1>OPERATION:</h1>
+            <h2>Angels of Heaven</h2>
+            <h3>Unmanned Aerial Vehicle Information Interface</h3>
           </div>
         
           <img src={airforce} />
