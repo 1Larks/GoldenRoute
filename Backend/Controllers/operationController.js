b17bfdb0beea66fc45097f3d5e0fa82b1545af19
1Larks <oribenado06@gmail.com>
2024-05-24 00:16:15 +0300

    Fixed stractural flaws + Added functionality

--- Backend/Controllers/operationController.js
+++ Backend/Controllers/operationController.js
@@ -1,53 +1,30 @@
 const {prisma} = require('../db');
-const getLast10OperationsService = require("../Services/operationService.js");
+const {getOperationsService, saveOperationService} = require("../Services/operationService.js");
 
 // Function to save a new operation
-const saveOperation = async (req, res) => {
+const saveOperationController = async (req, res) => {
   const { hostile_plane, planes } = req.body;
-  try {
-    const operation = await prisma.operation.create({
-      data: {
-        hostile_plane,
-        planes,
-      },
-    });
-    res.status(201).json(operation);
-  } catch (err) {
-    console.error(err.message);
+  const result = await saveOperationService(hostile_plane, planes);
+  if (result instanceof Error){
     res.status(500).json({ error: 'Server error' });
+  }
+  else{
+    res.status(201).json(result);
   }
 };
 
-// Function to get an operation by ID
-const getOperation = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const operation = await operationDB.findUnique({
-      where: { id: parseInt(id) },
-    });
-    if (!operation) {
-      return res.status(404).json({ error: 'Operation not found' });
-    }
-    res.status(200 ).json(operation);
-  } catch (err) {
-    console.error(err.message);
+const getOperationsController = async (req, res) => {
+  const { amount } = req.query;
+  result = await getOperationsService(amount);
+  if (result instanceof Error){
     res.status(500).json({ error: 'Server error' });
   }
-};
-
-// Get the last 10 operations with superficial information
-const getLast10OperationsController = async (req, res) => {
-  try {
-    const operations = await getLast10OperationsService();
-    res.json(operations);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server error' });
+  else{
+    res.status(200).json(result);
   }
 };
 
 module.exports = {
-  saveOperation,
-  getOperation,
-  getLast10OperationsController
+  saveOperationController,
+  getOperationsController
 };
