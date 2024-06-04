b956403337c76a81a035b8ae8742d6a527255878
1Larks <oribenado06@gmail.com>
2024-06-04 10:12:28 +0300

    input box can now only hold digits and decimal points

--- Frontend/src/Components/Input/InputSection.jsx
+++ Frontend/src/Components/Input/InputSection.jsx
@@ -1,17 +1,48 @@
 import getPlanesFromAPI from './getPlanesFromAPI.js'
 import InputBox from './TextInput.jsx';
 import './Input.css'
+import { useState } from 'react';
 
-const InputSection = ({URL, handleChange, setIsPlane, setExpandedIndex,  hostileInfo, setPlaneData, setHostileBackup, setPlaneBackup }) => {
-    return(
+const InputSection = ({URL, setIsPlane, setExpandedIndex,setHostileInfo, hostileInfo, setPlaneData, setHostileBackup, setPlaneBackup }) => {
+  
+  // The hostileInputHolder variable is a middle man between the text input boxes and the actual hostile plane's data.
+  const [hostileInfoHolder, setHostileInfoHolder] = useState({
+    Latitude: '',
+    Longitude: '',
+    Radius: '',
+    Speed: ''
+  });
+
+  const updateHostileInfo = () => {
+    setHostileInfo(hostileInfoHolder);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setHostileInfoHolder(prevState => ({
+          ...prevState,
+          [name]: value
+        }));
+  };
+
+  const handleKeyPress = (e) => {
+    const allowedKeys = /^[0-9.]$/;
+    if (!allowedKeys.test(e.key)) {
+      e.preventDefault();
+    }
+  };
+
+  return(
+
     <div className='InputSection'>
-      <InputBox InputName={"Latitude"} DisplayName={"קו רוחב"} onChange={handleChange} />,
-      <InputBox InputName={"Longitude"} DisplayName={"קו אורך"} onChange={handleChange} />,
-      <InputBox InputName={"Radius"} DisplayName={"רדיוס טיסה"} onChange={handleChange} />,
-      <InputBox InputName={"Speed"} DisplayName={"מהירות"} onChange={handleChange} />,
+      <InputBox InputName={"Latitude"} DisplayName={"קו רוחב"} onChange={handleChange} onKey={handleKeyPress}/>,
+      <InputBox InputName={"Longitude"} DisplayName={"קו אורך"} onChange={handleChange} onKey={handleKeyPress}/>,
+      <InputBox InputName={"Radius"} DisplayName={"רדיוס טיסה"} onChange={handleChange} onKey={handleKeyPress}/>,
+      <InputBox InputName={"Speed"} DisplayName={"מהירות"} onChange={handleChange} onKey={handleKeyPress}/>,
       <button onClick={() => {
         // The reason why we pass setIsPlane and setExpandedIndex is for the case that an error would accour so we dont want the app to crash.
-        getPlanesFromAPI(URL, hostileInfo, setPlaneData, setHostileBackup, setPlaneBackup, setIsPlane, setExpandedIndex);
+        updateHostileInfo()
+        getPlanesFromAPI(URL, hostileInfoHolder, setPlaneData, setHostileBackup, setPlaneBackup, setIsPlane, setExpandedIndex);
       }}>שלח מידע</button>
     </div>);
   }
