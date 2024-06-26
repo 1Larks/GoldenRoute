c400f5f1f2fa80f1190c76984ed6bb40383316b5
1Larks <oribenado06@gmail.com>
2024-06-26 21:09:46 +0300

    fixed docker, finished final steps

--- Backend/wait-for-it.sh
+++ Backend/wait-for-it.sh
@@ -8,10 +8,10 @@ host="$1"
 shift
 cmd="$@"
 
-until pg_isready -h "$host"; do
+until pg_isready -h "$host" -p 5432; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+  sleep 5
 done
 
 >&2 echo "Postgres is up - executing command"
-exec $cmd
+exec sh -c "$cmd"
